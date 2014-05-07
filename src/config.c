f67b0a9131bd230d25e2d3821ede2332e60a1198
Pierre Pronchery <khorben@defora.org>
2014-05-07 02:43:58 +0200

    Code cleanup

--- src/config.c
+++ src/config.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2005-2013 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2005-2014 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libSystem */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -346,7 +346,7 @@ int config_reset(Config * config)
 
 static void _delete_foreach(void const * key, void * value, void * data)
 {
-	char * str = (char*)key;
+	char * str = (char *)key;
 	Hash * hash = value;
 
 	free(str);
@@ -356,7 +356,7 @@ static void _delete_foreach(void const * key, void * value, void * data)
 
 static void _delete_foreach_section(void const * key, void * value, void * data)
 {
-	char * k = (char*)key;
+	char * k = (char *)key;
 	char * v = value;
 
 	free(k);
