870fc795d05e3a866760bfd626de9eac27c6a062
Pierre Pronchery <khorben@defora.org>
2014-11-16 01:42:07 +0100

    Re-indented the Python code

--- src/python/libSystem.py
+++ src/python/libSystem.py
@@ -21,48 +21,47 @@ import _libSystem
 
 #Config
 class Config:
-	def __init__(self):
-		self.config = _libSystem.config_new()
+    def __init__(self):
+        self.config = _libSystem.config_new()
 
-	def get(self, section, variable):
-		return _libSystem.config_get(self.config, section, variable)
+    def get(self, section, variable):
+        return _libSystem.config_get(self.config, section, variable)
 
-	def set(self, section, variable, value):
-		return _libSystem.config_set(self.config, section, variable,
-				value)
+    def set(self, section, variable, value):
+        return _libSystem.config_set(self.config, section, variable, value)
 
-	def load(self, filename):
-		return _libSystem.config_load(self.config, filename)
+    def load(self, filename):
+        return _libSystem.config_load(self.config, filename)
 
-	def reset(self):
-		return _libSystem.config_reset(self.config)
+    def reset(self):
+        return _libSystem.config_reset(self.config)
 
-	def save(self, filename):
-		return _libSystem.config_save(self.config, filename)
+    def save(self, filename):
+        return _libSystem.config_save(self.config, filename)
 
 
 #Event
 class Event:
-	def __init__(self):
-		self.event = _libSystem.event_new()
+    def __init__(self):
+        self.event = _libSystem.event_new()
 
-	def loop(self):
-		return _libSystem.event_loop(self.event)
+    def loop(self):
+        return _libSystem.event_loop(self.event)
 
 
 #Mutator
 class Mutator:
-	def __init__(self):
-		self.mutator = _libSystem.mutator_new()
+    def __init__(self):
+        self.mutator = _libSystem.mutator_new()
 
-	def get(self, key):
-		return _libSystem.mutator_get(self.mutator, key)
+    def get(self, key):
+        return _libSystem.mutator_get(self.mutator, key)
 
 
 #Plugin
 class Plugin:
-	def __init__(self):
-		self.plugin = _libSystem.plugin_new()
+    def __init__(self):
+        self.plugin = _libSystem.plugin_new()
 
-	def lookup(self, symbol):
-		return _libSystem.plugin_lookup(self.plugin, symbol)
+    def lookup(self, symbol):
+        return _libSystem.plugin_lookup(self.plugin, symbol)
