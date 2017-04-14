5eb805cf106220e63ffdc1bf7b9499483b1b31e9
Pierre Pronchery <khorben@defora.org>
2017-04-14 03:41:05 +0200

    Update the copyright notice

--- src/string.c
+++ src/string.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2005-2016 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2005-2017 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libSystem */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
