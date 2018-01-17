510c9020c7e9729151f84bbb49673e50a2887ea0
Pierre Pronchery <khorben@defora.org>
2018-01-17 03:28:43 +0100

    Use hexadecimal values when comparing ranges

--- src/variable.c
+++ src/variable.c
@@ -363,28 +363,28 @@ int variable_get_as(Variable * variable, VariableType type, void * result)
 				break;
 			}
 			if(variable->type == VT_UINT8
-					&& variable->u.uint8 < (1 << 7))
+					&& variable->u.uint8 <= 0x7f)
 				i8 = variable->u.uint8;
 			else if(variable->type == VT_INT16
-					&& variable->u.int16 >= -128
-					&& variable->u.int16 < (1 << 7))
+					&& variable->u.int16 >= (-0x7f-1)
+					&& variable->u.int16 <= 0x7f)
 				i8 = variable->u.int16;
 			else if(variable->type == VT_UINT16
-					&& variable->u.uint16 < (1 << 7))
+					&& variable->u.uint16 <= 0x7f)
 				i8 = variable->u.uint16;
 			else if(variable->type == VT_INT32
-					&& variable->u.int32 >= -128
-					&& variable->u.int32 < (1 << 7))
+					&& variable->u.int32 >= (-0x7f-1)
+					&& variable->u.int32 <= 0x7f)
 				i8 = variable->u.int32;
 			else if(variable->type == VT_UINT32
-					&& variable->u.uint32 < (1 << 7))
+					&& variable->u.uint32 <= 0x7f)
 				i8 = variable->u.uint32;
 			else if(variable->type == VT_INT64
-					&& variable->u.int64 >= -128
-					&& variable->u.int64 < (1 << 7))
+					&& variable->u.int64 >= (-0x7f-1)
+					&& variable->u.int64 <= 0x7f)
 				i8 = variable->u.int64;
 			else if(variable->type == VT_UINT64
-					&& variable->u.uint64 < (1 << 7))
+					&& variable->u.uint64 <= 0x7f)
 				i8 = variable->u.uint64;
 			else
 				/* FIXME implement more conversions */
@@ -404,25 +404,24 @@ int variable_get_as(Variable * variable, VariableType type, void * result)
 				u8 = variable->u.int8;
 			else if(variable->type == VT_INT16
 					&& variable->u.int16 >= 0
-					&& variable->u.int16 < (1 << 8))
+					&& variable->u.int16 <= 0xff)
 				u8 = variable->u.int16;
 			else if(variable->type == VT_UINT16
-					&& variable->u.uint16 < (1 << 8))
+					&& variable->u.uint16 <= 0xff)
 				u8 = variable->u.uint16;
 			else if(variable->type == VT_INT32
 					&& variable->u.int32 >= 0
-					&& variable->u.int32 < (1 << 8))
+					&& variable->u.int32 <= 0xff)
 				u8 = variable->u.int32;
 			else if(variable->type == VT_UINT32
-					&& variable->u.uint32
-					< (1 << 8))
+					&& variable->u.uint32 <= 0xff)
 				u8 = variable->u.uint32;
 			else if(variable->type == VT_INT64
 					&& variable->u.int64 >= 0
-					&& variable->u.int64 < (1 << 8))
+					&& variable->u.int64 <= 0xff)
 				u8 = variable->u.int64;
 			else if(variable->type == VT_UINT64
-					&& variable->u.uint64 < (1 << 8))
+					&& variable->u.uint64 <= 0xff)
 				u8 = variable->u.uint64;
 			else
 				/* FIXME implement more conversions */
@@ -437,25 +436,25 @@ int variable_get_as(Variable * variable, VariableType type, void * result)
 				break;
 			}
 			if(variable->type == VT_UINT16
-					&& variable->u.uint16 < (1 << 15))
+					&& variable->u.uint16 <= 0x7fff)
 				i16 = variable->u.uint16;
 			else if(variable->type == VT_INT8)
 				i16 = variable->u.int8;
 			else if(variable->type == VT_UINT8)
 				i16 = variable->u.uint8;
 			else if(variable->type == VT_INT32
-					&& variable->u.int32 >= -32768
-					&& variable->u.int32 < (1 << 15))
+					&& variable->u.int32 >= (-0x7fff-1)
+					&& variable->u.int32 <= 0x7fff)
 				i16 = variable->u.int32;
 			else if(variable->type == VT_UINT32
-					&& variable->u.uint32 < (1 << 15))
+					&& variable->u.uint32 <= 0x7fff)
 				i16 = variable->u.uint32;
 			else if(variable->type == VT_INT64
-					&& variable->u.int64 >= -32768
-					&& variable->u.int64 < (1 << 15))
+					&& variable->u.int64 >= (-0x7fff-1)
+					&& variable->u.int64 <= 0x7fff)
 				i16 = variable->u.int64;
 			else if(variable->type == VT_UINT64
-					&& variable->u.uint64 < (1 << 15))
+					&& variable->u.uint64 <= 0x7fff)
 				i16 = variable->u.uint64;
 			else
 				/* FIXME implement more conversions */
@@ -478,17 +477,17 @@ int variable_get_as(Variable * variable, VariableType type, void * result)
 				u16 = variable->u.uint8;
 			else if(variable->type == VT_INT32
 					&& variable->u.int32 >= 0
-					&& variable->u.int32 < (1 << 16))
+					&& variable->u.int32 <= 0xffff)
 				u16 = variable->u.int32;
 			else if(variable->type == VT_UINT32
-					&& variable->u.uint32 < (1 << 16))
+					&& variable->u.uint32 <= 0xffff)
 				u16 = variable->u.uint32;
 			else if(variable->type == VT_INT64
 					&& variable->u.int64 >= 0
-					&& variable->u.int64 < (1 << 16))
+					&& variable->u.int64 <= 0xffff)
 				u16 = variable->u.int64;
 			else if(variable->type == VT_UINT64
-					&& variable->u.uint64 < (1 << 16))
+					&& variable->u.uint64 <= 0xffff)
 				u16 = variable->u.uint64;
 			else
 				/* FIXME implement more conversions */
@@ -503,8 +502,7 @@ int variable_get_as(Variable * variable, VariableType type, void * result)
 				break;
 			}
 			if(variable->type == VT_UINT32
-					&& variable->u.uint32
-					< (uint32_t)1 << 31)
+					&& variable->u.uint32 <= 0x7fffffff)
 				i32 = variable->u.uint32;
 			else if(variable->type == VT_INT8)
 				i32 = variable->u.int8;
@@ -514,6 +512,13 @@ int variable_get_as(Variable * variable, VariableType type, void * result)
 				i32 = variable->u.int16;
 			else if(variable->type == VT_UINT16)
 				i32 = variable->u.uint16;
+			else if(variable->type == VT_INT64
+					&& variable->u.int64 >= (-0x7fffffff-1)
+					&& variable->u.int64 <= 0x7fffffff)
+				i32 = variable->u.int64;
+			else if(variable->type == VT_UINT64
+					&& variable->u.uint64 <= 0x7fffffff)
+				i32 = variable->u.uint64;
 			else
 				/* FIXME implement more conversions */
 				break;
@@ -539,6 +544,13 @@ int variable_get_as(Variable * variable, VariableType type, void * result)
 				u32 = variable->u.int16;
 			else if(variable->type == VT_UINT16)
 				u32 = variable->u.uint16;
+			else if(variable->type == VT_INT64
+					&& variable->u.int64 >= 0
+					&& variable->u.int64 <= 0xffffffff)
+				u32 = variable->u.int64;
+			else if(variable->type == VT_UINT64
+					&& variable->u.uint64 <= 0xffffffff)
+				u32 = variable->u.uint64;
 			else
 				/* FIXME implement more conversions */
 				break;
