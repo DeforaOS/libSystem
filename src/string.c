82ff2c1e6f546ee71e3c48c12cb6e4be4f6beae0
Pierre Pronchery <khorben@defora.org>
2021-01-09 18:23:26 +0100

    Update the copyright notice

--- src/string.c
+++ src/string.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2005-2020 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2005-2021 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libSystem */
 /* All rights reserved.
  *
