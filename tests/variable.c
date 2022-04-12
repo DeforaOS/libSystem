b9bb02814e2bf34d50670d337d56e5fa5c71cc8a
Pierre Pronchery <khorben@defora.org>
2022-04-12 22:18:53 +0200

    Update the copyright notice

--- tests/variable.c
+++ tests/variable.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2012-2021 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2012-2022 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libSystem */
 /* All rights reserved.
  *
